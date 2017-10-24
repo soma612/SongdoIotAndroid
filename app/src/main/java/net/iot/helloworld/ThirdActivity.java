50cd6182ed5a1cc96da99a2e2b29149008231517
Jinsu <noll0612@naver.com>
2017-10-24 15:35:50 +0900

    test4

--- app/src/main/java/net/iot/helloworld/ThirdActivity.java
+++ app/src/main/java/net/iot/helloworld/ThirdActivity.java
@@ -1,7 +1,11 @@
 package net.iot.helloworld;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -9,5 +13,24 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+
+    }
+
+    public void clickButton1(View view)
+    {
+        Toast.makeText(ThirdActivity.this,"이것은 토스트 메세지입니다.", Toast.LENGTH_SHORT).show();
+    }
+    public void clickButton2(View view)
+    {
+        AlertDialog.Builder dialog = new AlertDialog.Builder(ThirdActivity.this);
+        dialog.setTitle("알림");
+        dialog.setMessage("이것은 알림입니다.");
+        dialog.setPositiveButton("닫기", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        dialog.show();
     }
 }
